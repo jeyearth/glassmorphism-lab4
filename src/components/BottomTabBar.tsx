f31d64ba5f6934f1ed9e556cc5d28ae48021811a
JEY HIRANO <mrjr0722@me.com>
2026-05-08 10:29:38 +0900

    fix: update BottomTabBar button sizes and height for better UI

--- src/components/BottomTabBar.tsx
+++ src/components/BottomTabBar.tsx
@@ -9,12 +9,12 @@ export const BottomTabBar = () => {
       className="fixed bottom-0 w-full z-10 border-t border-white/20 pb-safe"
       style={glassStyle}
     >
-      <div className="flex justify-around items-center h-16">
-        <button className="p-2 text-black"><Home className="w-6 h-6" /></button>
-        <button className="p-2 text-gray-400"><Compass className="w-6 h-6" /></button>
-        <button className="p-2 text-gray-400"><PlusSquare className="w-6 h-6" /></button>
-        <button className="p-2 text-gray-400"><Heart className="w-6 h-6" /></button>
-        <button className="p-2 text-gray-400"><User className="w-6 h-6" /></button>
+      <div className="flex justify-around items-center h-20">
+        <button className="p-2 text-black"><Home className="w-8 h-8" /></button>
+        <button className="p-2 text-gray-400"><Compass className="w-8 h-8" /></button>
+        <button className="p-2 text-gray-400"><PlusSquare className="w-8 h-8" /></button>
+        <button className="p-2 text-gray-400"><Heart className="w-8 h-8" /></button>
+        <button className="p-2 text-gray-400"><User className="w-8 h-8" /></button>
       </div>
     </div>
   );
